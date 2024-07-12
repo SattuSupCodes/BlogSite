b9ea97d96387504e738a8cd9d58d8df6def053c2
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-12 14:03:11 +0530

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 app=Flask(__name__)
 app.secret_key="your_secret_key"
 
